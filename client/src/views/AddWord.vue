c8ed0ca5eaac089178cdc63c03198a53c4d409bf
Sadık Ünal <retaler_21@hotmail.com>
2022-08-31 21:22:32 +0300

    AddWord sayfasindaki grid component olarak duzenlendi ve eklendi.

--- client/src/views/AddWord.vue
+++ client/src/views/AddWord.vue
@@ -1,97 +1,20 @@
+<script lang="ts">
+import WordsTable from "../components/WordsTable.vue";
+export default {
+  component: {
+    WordsTable,
+  },
+};
+</script>
 <template>
   <div class="addword-container">
     <div class="word-item-1">Kelimeler</div>
     <div class="word-item-2">
-      <div class="word-btn">
-        <button class="btn-add">Ekle</button>
-      </div>
-      <div class="word-container">
-        <div class="word-header">
-          <div class="word-firstLangHeader">Dil 1</div>
-          <div class="word-secondLangHeader">Dil 2</div>
-        </div>
-        <div class="word-list">
-          <div class="word-list-item">
-            <div class="word-item-firstLang">Yazi1</div>
-            <div class="word-item-secondLang">Text1</div>
-            <div class="word-item-btns">
-              <button class="word-item-btn">Düzenle</button>
-              <button class="word-item-btn">Sil</button>
-            </div>
-          </div>
-          <div class="word-list-item">
-            <div class="word-item-firstLang">Yazi2</div>
-            <div class="word-item-secondLang">Text2</div>
-            <div class="word-item-btns">
-              <button class="word-item-btn">Düzenle</button>
-              <button class="word-item-btn">Sil</button>
-            </div>
-          </div>
-          <div class="word-list-item">
-            <div class="word-item-firstLang">Yazi2</div>
-            <div class="word-item-secondLang">Text2</div>
-            <div class="word-item-btns">
-              <button class="word-item-btn">Düzenle</button>
-              <button class="word-item-btn">Sil</button>
-            </div>
-          </div>
-          <div class="word-list-item">
-            <div class="word-item-firstLang">Yazi2</div>
-            <div class="word-item-secondLang">Text2</div>
-            <div class="word-item-btns">
-              <button class="word-item-btn">Düzenle</button>
-              <button class="word-item-btn">Sil</button>
-            </div>
-          </div>
-          <div class="word-list-item">
-            <div class="word-item-firstLang">Yazi2</div>
-            <div class="word-item-secondLang">Text2</div>
-            <div class="word-item-btns">
-              <button class="word-item-btn">Düzenle</button>
-              <button class="word-item-btn">Sil</button>
-            </div>
-          </div>
-          <div class="word-list-item">
-            <div class="word-item-firstLang">Yazi2</div>
-            <div class="word-item-secondLang">Text2</div>
-            <div class="word-item-btns">
-              <button class="word-item-btn">Düzenle</button>
-              <button class="word-item-btn">Sil</button>
-            </div>
-          </div>
-          <div class="word-list-item">
-            <div class="word-item-firstLang">Yazi2</div>
-            <div class="word-item-secondLang">Text2</div>
-            <div class="word-item-btns">
-              <button class="word-item-btn">Düzenle</button>
-              <button class="word-item-btn">Sil</button>
-            </div>
-          </div>
-          <div class="word-list-item">
-            <div class="word-item-firstLang">Yazi2</div>
-            <div class="word-item-secondLang">Text2</div>
-            <div class="word-item-btns">
-              <button class="word-item-btn">Düzenle</button>
-              <button class="word-item-btn">Sil</button>
-            </div>
-          </div>
-          <div class="word-list-item">
-            <div class="word-item-firstLang">Yazi2</div>
-            <div class="word-item-secondLang">Text2</div>
-            <div class="word-item-btns">
-              <button class="word-item-btn">Düzenle</button>
-              <button class="word-item-btn">Sil</button>
-            </div>
-          </div>
-          <div class="last-item-bottom"></div>
-        </div>
-      </div>
+      <WordsTable />
     </div>
   </div>
 </template>
-<script lang="ts">
-export default {};
-</script>
+
 <style scoped>
 .addword-container {
   display: grid;
@@ -111,74 +34,5 @@ export default {};
 .word-item-1 {
   padding-top: 1.5vh;
   min-height: 38px;
-}
-.btn-add {
-  background-color: var(--dkblue);
-  color: white;
-  cursor: pointer;
-  font-size: 1.4rem;
-  border: 1px;
-  border-radius: 1rem;
-  height: 2.5rem;
-}
-.word-btn {
-  padding-top: 1.5rem;
-  padding-right: 4rem;
-  text-align: end;
-}
-.word-container {
-  margin-top: 1rem;
-  font-size: 1.4rem;
-}
-.word-header {
-  display: flex;
-  align-items: center;
-  padding-bottom: 0.8rem;
-  justify-content: space-between;
-}
-.word-firstLangHeader,
-.work-secondLangHeader {
-  width: 4rem;
-}
-.word-firstLangHeader {
-  padding-left: 10rem;
-  padding-right: 14rem;
-  
-}
-.word-secondLangHeader {
-  padding-right: 39rem;
-  width: 4rem;
-}
-.word-list-item {
-  display: flex;
-  align-items: center;
-  padding-top: 0.7rem;
-  padding-bottom: 0.7rem;
-  border-top: 1px solid #9eb5cc;
-  justify-content: space-between;
-}
-.last-item-bottom {
-  border-top: 1px solid #9eb5cc;
-}
-.word-item-firstLang {
-  padding-left: 10rem;
-  padding-right: 1rem;
-}
-.word-item-secondLang {
-  padding-right: 10rem;
-}
-.word-item-btns {
-  padding-right: 5rem;
-  padding-left: 0.7rem;
-}
-.word-item-btn {
-  background-color: var(--dkblue);
-  color: white;
-  cursor: pointer;
-  font-size: 1.4rem;
-  border: 1px;
-  border-radius: 1rem;
-  margin: 6px;
-  box-sizing: border-box;
 }
 </style>
