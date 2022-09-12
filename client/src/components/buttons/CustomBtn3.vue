d6e36c0da57b09dba289d0d66fb3af2526064f83
Sadık Ünal <retaler_21@hotmail.com>
2022-09-13 00:06:43 +0300

    Butonlarin bir kismi componentlere donusturuldu ve projeye eklendiler.

---
+++ client/src/components/buttons/CustomBtn3.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="customBtn3">{{ btnName }}</button>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    btnName: String,
+  },
+};
+</script>
+<style scoped>
+.customBtn3 {
+  background-color: var(--dkblue);
+  color: white;
+  cursor: pointer;
+  font-size: 1.4rem;
+  border: 1px;
+  border-radius: 1rem;
+  height: 2.5rem;
+  box-shadow: 0 2px 0 0 #000;
+}
+.customBtn3:active {
+  box-shadow: 0 2px #000;
+  transform: translateY(1px);
+}
+.customBtn3:hover {
+  background-color: rgb(21, 72, 168);
+}
+</style>
