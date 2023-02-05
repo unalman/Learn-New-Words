4197c8b9706e3f4005ff5c8d71c0be43ccc6a93a
Sadık Ünal <retaler_21@hotmail.com>
2023-02-05 18:20:21 +0300

    UI daki bazı sorunlar giderildi.

--- client/src/components/buttons/CustomBtn1.vue
+++ client/src/components/buttons/CustomBtn1.vue
@@ -12,13 +12,17 @@ export default {
 .customBtn1 {
   color: white;
   display: inline-block;
-  background-color: rgb(106 146 205);
+  background-color: #5fa7db;
   font-size: 1.5rem;
   border-radius: 0.3rem;
   padding: 3px;
 }
-div .customBtn1:hover {
-  background-color: rgb(38 105 205);
-  text-decoration: none;
+.customBtn1:hover {
+  background-color: rgb(70 119 211);
+}
+.customBtn1:active {
+  background-color: rgb(70 119 211);
+  box-shadow: 0 1px rgb(70 119 211);
+  transform: translateY(1px);
 }
 </style>
